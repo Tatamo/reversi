e09887471c1c99ef247b58491040f0a619fb1554
Tatamo <nsorder@gmail.com>
2020-03-04 00:56:49 +0900

    add function to check placable or not

--- Program.fs
+++ Program.fs
@@ -5,4 +5,9 @@ open Reversi
 
 [<EntryPoint>]
 let main argv =
+  printfn "%A" (seq {for x in 0..7 do for y in 1..7 do yield (x,y)} |> Seq.filter (fun pos -> checkPlacable initBoard Black pos))
+  printfn "%b" (checkPlacable initBoard Black (1, 1))
+  printfn "%b" (checkPlacable initBoard Black (2, 3))
+  printfn "%b" (checkPlacable initBoard Black (3, 2))
+  printfn "%b" (checkPlacable initBoard Black (3, 3))
   0 // return an integer exit code
--- Reversi.fs
+++ Reversi.fs
@@ -4,12 +4,12 @@ type Color =
     | Black
     | White
 
-type Disk = Color option
-
 let swap color =
     match color with
     | Black -> White
     | White -> Black
+
+type Disk = Color option
 
 type Board = Disk [,]
 
@@ -22,6 +22,47 @@ let initPlacer x y =
     | _ -> None
 
 let initBoard: Board = Array2D.init 8 8 initPlacer
+
+let directions =
+    seq {
+        for x in -1 .. 1 do
+            for y in -1 .. 1 do
+                if not (x = 0 && y = 0) then yield (x, y)
+    }
+
+let isInBoard (x, y) = x >= 0 && x < 8 && y >= 0 && y < 8
+
+/// pick up disks, starting from (x,y) and go to the specified direction toward the end of board
+let rec extractLine (board: Board) (x, y) dir =
+    if isInBoard (x, y) then
+        match dir with
+        | (dx, dy) -> board.[x, y] :: extractLine board (x + dx, y + dy) dir
+    else
+        []
+
+let rec _checkFlippableLine disks color =
+    match disks with
+    | Some(c) :: remains ->
+        if c = color then true else _checkFlippableLine remains color
+    | None :: remains -> false
+    | [] -> false
+
+let checkFlippableLine disks color =
+    match disks with
+    | Some(c) :: remains ->
+        if c = color then false else _checkFlippableLine remains color
+    | None :: remains -> false
+    | [] -> false
+
+let checkPlacable (board: Board) color (x, y) =
+    if not (isInBoard (x, y)) then
+        false
+    else
+        match board.[x, y] with
+        | Some(_) -> false
+        | None ->
+            directions
+            |> Seq.exists (fun (dx, dy) -> checkFlippableLine (extractLine board (x + dx, y + dy) (dx, dy)) color)
 
 type GameStatus =
     | InGame of Color
